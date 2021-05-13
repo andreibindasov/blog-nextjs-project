860e053403c71a78d127370c7a3abe48137d85db
andreibindasov <43188171+andreibindasov@users.noreply.github.com>
2021-05-13 15:54:43 -0600

    env var & storing in db added

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb'
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const {email, name, message} = req.body
 
@@ -20,7 +22,30 @@ function handler(req, res) {
       message
     }
 
-    console.log(newMessage)
+    let client
+
+    try {
+      client = await MongoClient.connect(`mongodb+srv://andrei:${process.env.DB_SECRET}@cluster0.cx3nb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, 
+                      {useNewUrlParser: true, useUnifiedTopology: true})      
+    } catch(error) {
+      console.log(error)
+      res.status(500).json({message: 'Could not connect to database'})
+      return
+    }
+
+    const db = client.db()
+
+    try{
+      const result = await db.collection('messages').insertOne(newMessage)
+      newMessage.id = result.insertedId
+    } catch(error){
+      client.close()
+      console.log(error)
+      res.status(500).json({message: 'Could not perform the operation'})
+      return
+    }
+
+    client.close()
 
     res.status(201)
        .json({message: 'Successfully stored!', message: newMessage})
